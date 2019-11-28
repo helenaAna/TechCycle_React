c1c4e60d442d648ab404c74b43921e05cc163790
FIC <FIC@info.local>
2019-11-28 17:42:34 -0200

    Versão 6

---
+++ src/pages/PainelAdm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CabecalhoAdm from '../componentes/CabecalhoAdm';
+import Rodape from '../componentes/Rodape';
+
+
+function PainelAdm() {
+  return (
+    <div className="App">
+      <CabecalhoAdm/>
+ 
+      <Rodape/>
+    </div>
+  );
+}
+
+export default PainelAdm;
