d5aa21e16e0e9db2cfe575d1a695551c3ef2cf73
Vivian <vigveloso@hotmail.com>
2019-12-14 16:27:00 -0300

    Alteração cadastro

--- src/index.js
+++ src/index.js
@@ -23,12 +23,8 @@ import ListaInteresse from './pages/ListaInteresse';
 import ListaProduto from './pages/ListaProduto';
 import PerfilUsuario from './pages/PerfilUsuario';
 import CadastroProduto from './pages/CadastroProduto';
-<<<<<<< HEAD
-import ListaUsuario from './pages/ListaUsuario';
-=======
 import ListaUsuario from './pages/ListarUsuarios';
 import Aprovacoes from './pages/Aprovacoes';
->>>>>>> 6df2744f822ff99ed50fcd798ea94e4350b85ce4
 
 // const PermissaoLogar = ({component : Component}) => (
 //     <Route
--- src/pages/CadastroAnuncio.js
+++ src/pages/CadastroAnuncio.js
@@ -23,7 +23,9 @@ class CadastroAnuncio extends Component {
                 listaAnuncio: [],
                 cadastrarPreco: '',
                 dataExpiracao: '',
-                classificacao: ''
+                descricao : '',
+                classificacao: '',
+                foto : '',
             },
 
             fileInput: React.createRef()
@@ -46,16 +48,16 @@ class CadastroAnuncio extends Component {
     cadastraAnuncio = (e) => {
         e.preventDefault();
 
-        // let anuncio = new FormData();
+        let anuncio = new FormData();
 
-        // anuncio.set("cadastrarPreco", this.state.cadastraAnuncio.cadastrarPreco);
-        // anuncio.set("dataExpiracao", this.state.cadastraAnuncio.dataExpiracao);
+        anuncio.set("foto", this.state.foto.current.files[0]);
+        anuncio.set("descricao", this.state.postAnuncio.descricao);
+        anuncio.set("dataExpiracao", this.state.postAnuncio.dataExpiracao);
+        anuncio.set("preco", this.state.postAnuncio.cadastrarPreco);
 
         fetch('http://localhost:5000/api/anuncio', {
             method: "POST",
-            body: JSON.stringify({dataExpiracao: this.state.postAnuncio.dataExpiracao,
-            cadastrarPreco: this.state.postAnuncio.cadastrarPreco
-            })
+            body: JSON.stringify({anuncio})
         })
             .then(response => response.json())
             .then(response => {
@@ -137,55 +139,35 @@ class CadastroAnuncio extends Component {
 
                                         <div className="amostra_do_produto_cdu1">
 
-                                        <div className="usuario_secao_imagem_cad1">
-                                <img clasName="imgcda123" src={require("../assets/img/camera.svg")} />
-                            </div>
+                                            <div className="usuario_secao_imagem_cad1">
+                                                <img clasName="imgcda123" src={require("../assets/img/camera.svg")} />
+                                            </div>
 
 
-                            <div class='input-wrapper'>
-                                <label for='input-file'> <i className="fas fa-upload"></i>     Selecionar um arquivo</label>
-                                <input id='input-file' type='file'
-                                    arial-label="coloque sua foto"
-                                    ref={this.state.foto}
-                                />
-                                <span id='file-name'></span>
-                            </div>
+                                            <div class='input-wrapper'>
+                                                <label for='input-file'> <i className="fas fa-upload"></i>     Selecionar um arquivo</label>
+                                                <input id='input-file' type='file'
+                                                    arial-label="coloque sua foto"
+                                                    ref={this.state.foto}
+                                                />
+                                                <span id='file-name'></span>
+                                            </div>  
 
                                         </div> 
-                                        <div className="amostra_descricao_cdu" >
-                                            <p><span>Descrição do produto:</span></p>
-                                            <h2>{this.state.produtoSelecionado.nomeProduto}</h2>
-                                            <p>Número do modelo: {this.state.produtoSelecionado.modelo}</p>
-                                            <p> {this.state.produtoSelecionado.descricao}</p>
-                                            <p>Memória: {this.state.produtoSelecionado.memoria}</p>
-                                        </div>
+                                            <div className="amostra_descricao_cdu" >
+                                                <p><span>Descrição do produto:</span></p>
+                                                <h2>{this.state.produtoSelecionado.nomeProduto}</h2>
+                                                <p>Número do modelo: {this.state.produtoSelecionado.modelo}</p>
+                                                <p> {this.state.produtoSelecionado.descricao}</p>
+                                                <p>Memória: {this.state.produtoSelecionado.memoria}</p>
+                                            </div>
 
                                     </section>
                                     <section className="formulario_anuncio_cdu">
-
-                                        <div className="preco_expiracao_cdu">
-                                            {/* <form action="#" method="POST" className="selecaodoanuncio_cdu"> */}
-                                            <div className="campos">
-                                                <label for="campo_preco">Preço do equipamento:</label>
-                                                <input onChange={this.atualizaState}
-                                                    name="campo_preco"
-                                                    type="text"
-                                                    alt="Preço..."
-                                                    placeholder="Preço do equipamento"
-                                                    className="barra_preco_expiracao_cdu" />
-                                            </div>
-<<<<<<< HEAD
-                                        )
-
-                                    }
-                                    )
-                                } */}
-
                             </section>
-                            <section className="formulario_anuncio_cdu">
+                            <section className="formulario_anuncio_cdu5">
 
-                                <div className="preco_expiracao_cdu">
-                                    <form action="#" method="POST" className="selecaodoanuncio_cdu" onSubmit = {this.cadastraAnuncio.bind()}>
+                                <div className="preco_expiracao_cdu_div">
                                         <div className="campos">
                                             <label for="campo_preco">Preço do equipamento:</label>
                                             <input 
@@ -195,105 +177,75 @@ class CadastroAnuncio extends Component {
                                             alt="Preço..." 
                                             placeholder="Preço do equipamento"
                                             className="barra_preco_expiracao_cdu" />
-=======
-                                            {/* </form> */}
->>>>>>> 6df2744f822ff99ed50fcd798ea94e4350b85ce4
                                         </div>
 
                                         <div className="preco_expiracao_cdu">
-                                            <form action="#" method="POST" className="selecaodoanuncio_cdu">
                                                 <div className="campos">
                                                     <label for="campo_expiracao">Data de expiração do anúncio:</label>
                                                     <input onChange={this.atualizaState} name="campo_expiracao" type="data" alt="Data de expiração"
                                                         placeholder="Data de expiração" className="barra_preco_expiracao_cdu" />
                                                 </div>
-                                            </form>
                                         </div>
 
                                         <div className="informar_avaliacao">
-                                            {/* <form action="#" className="avaliacao"> */}
                                             <legend>Avaliação do equipamento:</legend>
-                                            <div className="tipos_avaliacao">
-                                                
-                                                {/* <div className="avaliacao_estrelas">
-                                                    <span className="fa fa-star checked"></span>
-                                                    <span className="fa fa-star checked"></span>
-                                                    <span className="fa fa-star checked"></span>
-                                                </div>
-                                                <div className="avaliacao_estrelas">
-                                                    <span className="fa fa-star checked"></span>
-                                                    <span className="fa fa-star checked"></span>
-                                                </div>
-                                                <div className="avaliacao_estrelas">
-                                                    <span className="fa fa-star checked"></span>
-                                                </div> */}
+                                            <div className="tipos_avaliacao2">
 
-
-
-<form className="rating-form" action="#" method="post" name="rating-movie">
-  <fieldset className="form-group">
-    
-    <legend className="form-legend">Rating:</legend>
-    
-    <div className="form-item">
-      
-      <input id="rating-5" name="rating" type="radio" value="5" />
-      <label for="rating-5" data-value="5">
-        <span className="rating-star">
-          <i className="fa fa-star-o"></i>
-          <i className="fa fa-star"></i>
-        </span>
-        <span className="ir">5</span>
-      </label>
-      <input id="rating-4" name="rating" type="radio" value="4" />
-      <label for="rating-4" data-value="4">
-        <span className="rating-star">
-          <i className="fa fa-star-o"></i>
-          <i className="fa fa-star"></i>
-        </span>
-        <span className="ir">4</span>
-      </label>
-      <input id="rating-3" name="rating" type="radio" value="3" />
-      <label for="rating-3" data-value="3">
-        <span className="rating-star">
-          <i className="fa fa-star-o"></i>
-          <i className="fa fa-star"></i>
-        </span>
-        <span className="ir">3</span>
-      </label>
-      <input id="rating-2" name="rating" type="radio" value="2" />
-      <label for="rating-2" data-value="2">
-        <span className="rating-star">
-          <i className="fa fa-star-o"></i>
-          <i className="fa fa-star"></i>
-        </span>
-        <span className="ir">2</span>
-      </label>
-      <input id="rating-1" name="rating" type="radio" value="1" />
-      <label for="rating-1" data-value="1">
-        <span className="rating-star">
-          <i className="fa fa-star-o"></i>
-          <i className="fa fa-star"></i>
-        </span>
-        <span className="ir">1</span>
-      </label>
-      
-      {/* <div className="form-action">
-        <input className="btn-reset" type="reset" value="Reset" />   
-      </div> */}
-
-      {/* <div className="form-output">
-        ? / 5
-      </div> */}
-      
-    </div>
-    
-  </fieldset>
-</form>
-
+                                        <div className="rating-form" action="#" method="post" name="rating-movie">
+                                        <fieldset className="form-group">
+                                            
+                                            <legend className="form-legend">Rating:</legend>
+                                            
+                                            <div className="form-item">
+                                            
+                                            <input id="rating-5" name="rating" type="radio" value="5" />
+                                            <label for="rating-5" data-value="5">
+                                                <span className="rating-star">
+                                                <i className="fa fa-star-o"></i>
+                                                <i className="fa fa-star"></i>
+                                                </span>
+                                                <span className="ir">5</span>
+                                            </label>
+                                            <input id="rating-4" name="rating" type="radio" value="4" />
+                                            <label for="rating-4" data-value="4">
+                                                <span className="rating-star">
+                                                <i className="fa fa-star-o"></i>
+                                                <i className="fa fa-star"></i>
+                                                </span>
+                                                <span className="ir">4</span>
+                                            </label>
+                                            <input id="rating-3" name="rating" type="radio" value="3" />
+                                            <label for="rating-3" data-value="3">
+                                                <span className="rating-star">
+                                                <i className="fa fa-star-o"></i>
+                                                <i className="fa fa-star"></i>
+                                                </span>
+                                                <span className="ir">3</span>
+                                            </label>
+                                            <input id="rating-2" name="rating" type="radio" value="2" />
+                                            <label for="rating-2" data-value="2">
+                                                <span className="rating-star">
+                                                <i className="fa fa-star-o"></i>
+                                                <i className="fa fa-star"></i>
+                                                </span>
+                                                <span className="ir">2</span>
+                                            </label>
+                                            <input id="rating-1" name="rating" type="radio" value="1" />
+                                            <label for="rating-1" data-value="1">
+                                                <span className="rating-star">
+                                                <i className="fa fa-star-o"></i>
+                                                <i className="fa fa-star"></i>
+                                                </span>
+                                                <span className="ir">1</span>
+                                            </label>
                                             </div>
-                                            {/* </form> */}
+                                        </fieldset>
                                         </div>
+
+                                        </div>
+                                        </div>
+                                        </div>
+                                        
                                     </section>
                                     <div className="botao_adicionar_cdu">
 
