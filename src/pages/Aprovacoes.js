0288811b9f2b5b33438bbe784ef2a406a42d6eee
helenaAna <54956855+helenaAna@users.noreply.github.com>
2019-12-16 00:44:16 -0200

    alteracoes

--- src/pages/Aprovacoes.js
+++ src/pages/Aprovacoes.js
@@ -3,12 +3,13 @@ import CabecalhoAdm from '../componentes/CabecalhoAdm';
 import Rodape from '../componentes/Rodape';
 
 import '../assets/css/padrao.css';
-import '../assets/css/listainteresse.css';
+import '../assets/css/listainteresse.css'
 
 class Aprovacoes extends Component{
    constructor(props){
        super(props);
        this.state ={
+           listaInteresse: [],
             usuario: {
                nome:'',
                departamento:''
@@ -21,8 +22,16 @@ class Aprovacoes extends Component{
        }
    }
 
-buscaUsuario(){
-
+   buscarUsuario(){
+    fetch("http://localhost:5000/api/usuario/buscarTipo/aguardandoaprovacao")
+    .then(response => response.json())
+    .then(data => {
+        this.setState({listaInteresse:data})
+    })
+    .catch((error) => console.log(error))
+}
+componentDidMount(){
+    this.buscarUsuario();
 }
 
 
@@ -32,7 +41,84 @@ buscaUsuario(){
             <div>
                 <CabecalhoAdm/>
                 <main>
-                    
+                <section classNameName="titulo_interesses">
+                    <h1>Lista de aprovações</h1>
+                    <hr/>
+                </section>
+                <nav className="nav_tabs">
+                    <ul>
+                        <li>
+                        <input type="radio" id="tab1" className="rd_tab" name="tabs" checked/>
+                        <label for="tab1" className="tab_label">Lista de Interesses</label>
+                        <div className="tab-content">
+                        <article>
+                            <section classNameName="lista_aprovados">
+                                <ul classNameName="lista_aprovados">
+                                    {
+                                        this.state.listainteresse.map(function (interesse) {
+                                            if (interesse.aprovado == "Sim") {
+                                                
+                                                return (
+                                                    
+                                                    <li classNameName="linha" value="{interesse.idInteresse}">
+                                                        <div classNameName="li_imagem">
+                                                            <img src={"http://localhost:5000/Resources/Anuncio/" + interesse.foto} alt="imagem do produto" />
+                                                        </div>
+                                                        {console.log(interesse.foto)}
+                                                        <div classNameName="li_titulo">
+                                                            <h3>{interesse.idAnuncioNavigation.idProdutoNavigation.nomeProduto}</h3>
+                                                        </div>
+                                                        <div classNameName="li_descricao">
+                                                            <p>{interesse.idAnuncioNavigation.idProdutoNavigation.descricao}</p>
+                                                        </div>
+                                                    </li>
+                                                )
+                                            }
+                                        }.bind(this))
+                                    }
+                                </ul>
+                            </section>
+                        </article> 
+                        </div>
+                        </li>
+                        <li>
+                        <input type="radio" id="tab2" className="rd_tab" name="tabs" checked> 
+                    </input>
+                    <label for="tab2" className="tab_label">Interesses sem resposta</label>
+
+                    <div className="tab-content">
+                        <article>
+                            <section classNameName="lista_aprovados">
+                                <ul classNameName="lista_aprovados">
+                                {
+                                    this.state.listainteresse.map(function (interesse) {
+                                        if (interesse.aprovado == "Não") {
+                                            
+                                            return (
+                                            
+                                                <li classNameName="linha" value="{interesse.idInteresse}">
+                                                    <div classNameName="li_imagem">
+                                                        <img src={"http://localhost:5000/Resources/Anuncio/" + interesse.foto} alt="imagem do produto" />
+                                                    </div>
+                                                    {console.log(interesse.foto)}
+                                                    <div classNameName="li_titulo">
+                                                        <h3>{interesse.idAnuncioNavigation.idProdutoNavigation.nomeProduto}</h3>
+                                                    </div>
+                                                    <div classNameName="li_descricao">
+                                                        <p>{interesse.idAnuncioNavigation.idProdutoNavigation.descricao}</p>
+                                                    </div>
+                                                </li>
+                                            )
+                                        }
+                                    }.bind(this))
+                                }
+                                </ul>
+                            </section>
+                        </article> 
+                    </div>
+                        </li>
+                    </ul> 
+                </nav>
                 </main>
                 <Rodape/>
             </div>
--- src/pages/ListaInteresse.js
+++ src/pages/ListaInteresse.js
@@ -44,37 +44,36 @@ class ListaInteresse extends Component {
             <CabecalhoUser />
 
             <main>
-            <section className="titulo_interesses">
+            <section classNameName="titulo_interesses">
                 <h1>Lista de Interesses</h1>
                 <hr></hr>
             </section>
 
-            <nav class="nav_tabs">
+            <nav className="nav_tabs">
 			<ul>
                 <li>
-                    <input type="radio" id="tab1" class="rd_tab" name="tabs" checked> 
+                    <input type="radio" id="tab1" className="rd_tab" name="tabs" checked> 
                     </input>
-                    <label for="tab1" class="tab_label">Interesses Aprovados</label>
-                    <div class="tab-content">
-                        <h2></h2>
+                    <label for="tab1" className="tab_label">Interesses Aprovados</label>
+                    <div className="tab-content">
                         <article>
-                            <section className="lista_aprovados">
-                                <ul className="lista_aprovados">
+                            <section classNameName="lista_aprovados">
+                                <ul classNameName="lista_aprovados">
                                     {
                                         this.state.listainteresse.map(function (interesse) {
                                             if (interesse.aprovado == "Sim") {
                                                 
                                                 return (
                                                     
-                                                    <li className="linha" value="{interesse.idInteresse}">
-                                                        <div className="li_imagem">
+                                                    <li classNameName="linha" value="{interesse.idInteresse}">
+                                                        <div classNameName="li_imagem">
                                                             <img src={"http://localhost:5000/Resources/Anuncio/" + interesse.foto} alt="imagem do produto" />
                                                         </div>
                                                         {console.log(interesse.foto)}
-                                                        <div className="li_titulo">
+                                                        <div classNameName="li_titulo">
                                                             <h3>{interesse.idAnuncioNavigation.idProdutoNavigation.nomeProduto}</h3>
                                                         </div>
-                                                        <div className="li_descricao">
+                                                        <div classNameName="li_descricao">
                                                             <p>{interesse.idAnuncioNavigation.idProdutoNavigation.descricao}</p>
                                                         </div>
                                                     </li>
@@ -88,29 +87,29 @@ class ListaInteresse extends Component {
                     </div>
                 </li>
                 <li>
-                    <input type="radio" id="tab2" class="rd_tab" name="tabs" checked> 
+                    <input type="radio" id="tab2" className="rd_tab" name="tabs" checked> 
                     </input>
-                    <label for="tab2" class="tab_label">Interesses sem resposta</label>
+                    <label for="tab2" className="tab_label">Interesses sem resposta</label>
 
-                    <div class="tab-content">
+                    <div className="tab-content">
                         <article>
-                            <section className="lista_aprovados">
-                                <ul className="lista_aprovados">
+                            <section classNameName="lista_aprovados">
+                                <ul classNameName="lista_aprovados">
                                 {
                                     this.state.listainteresse.map(function (interesse) {
                                         if (interesse.aprovado == "Não") {
                                             
                                             return (
                                             
-                                                <li className="linha" value="{interesse.idInteresse}">
-                                                    <div className="li_imagem">
+                                                <li classNameName="linha" value="{interesse.idInteresse}">
+                                                    <div classNameName="li_imagem">
                                                         <img src={"http://localhost:5000/Resources/Anuncio/" + interesse.foto} alt="imagem do produto" />
                                                     </div>
                                                     {console.log(interesse.foto)}
-                                                    <div className="li_titulo">
+                                                    <div classNameName="li_titulo">
                                                         <h3>{interesse.idAnuncioNavigation.idProdutoNavigation.nomeProduto}</h3>
                                                     </div>
-                                                    <div className="li_descricao">
+                                                    <div classNameName="li_descricao">
                                                         <p>{interesse.idAnuncioNavigation.idProdutoNavigation.descricao}</p>
                                                     </div>
                                                 </li>
